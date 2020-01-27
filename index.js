3af9a761deb460a47bcdc291050d167f0c1b1a9a
STAYCUTE <34066247+staycuteangelanddevil@users.noreply.github.com>
2020-01-27 22:25:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const config = require('./config.json')
 client.on('ready', () =>{
 console.log('Bot is operational')
 })
-
-client.on('message',(message) => {
-console.log(`${message.author.username} : ${message.content}`)
+client.on('message', async (message) => {
+console.log(`${message.author.username} : ${message.content}`) //log
+if (message.author.bot) return;
+if (message.channel.type === "dm") {
+    return;
+}
+if (message.content.indexOf(config.prefix) !== 0) return;
+var args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+var command = args.shift().toLowerCase();
+   try {
+        let commandFile = require(`./commands/${command}.js`);
+        commandFile.run(client, message, args, database);
+    } catch (err) {
+        console.log(err)
+    }
 })
 
-client.login('Your Token')
+client.login(config.token)
